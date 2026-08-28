25fce26351314deddd5e91e2a30db092b6df777c
Shamsfort <159244276+Shamsfort@users.noreply.github.com>
2026-08-28 19:40:59 +0200

    Aggiungi il connettore PostgreSQL compatibile con Neon

---
+++ app/Database/Connectors/NeonPostgresConnector.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Connectors;
+
+use Illuminate\Database\Connectors\PostgresConnector;
+
+class NeonPostgresConnector extends PostgresConnector
+{
+    protected function getDsn(array $config)
+    {
+        $dsn = parent::getDsn($config);
+        $endpointId = $config['neon_endpoint'] ?? null;
+
+        if (is_string($endpointId) && $endpointId !== '') {
+            $dsn .= ";options='endpoint={$endpointId}'";
+        }
+
+        return $dsn;
+    }
+}
+
