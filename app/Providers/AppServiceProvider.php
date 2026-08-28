18230a5174eea974bdd55ace6248035e9c0ad4a2
Shamsfort <159244276+Shamsfort@users.noreply.github.com>
2026-08-28 19:40:54 +0200

    Compatibilità Neon: aggiorna app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Database\Connectors\NeonPostgresConnector;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -9,6 +10,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->app->bind('db.connector.pgsql', fn () => new NeonPostgresConnector());
     }
 
     public function boot(): void
@@ -16,3 +18,4 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrapFive();
     }
 }
+
